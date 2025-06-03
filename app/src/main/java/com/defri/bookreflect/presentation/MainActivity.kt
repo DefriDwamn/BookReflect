4befd361b5049f7c75163652dc55ab2e980fe2cb
Yakov Novashinskii <Emailno@bk.ru>
2025-06-04 00:28:05 +0300

    feat: navigation for auth screens and home nav template

--- app/src/main/java/com/defri/bookreflect/presentation/MainActivity.kt
+++ app/src/main/java/com/defri/bookreflect/presentation/MainActivity.kt
@@ -8,10 +8,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.defri.bookreflect.presentation.auth.ForgotPasswordScreen
+import com.defri.bookreflect.presentation.auth.LoginScreen
+import com.defri.bookreflect.presentation.auth.RegisterScreen
 import com.defri.bookreflect.presentation.common.theme.BookReflectTheme
+import com.defri.bookreflect.presentation.home.HomeScreen
+import com.defri.bookreflect.presentation.profile.ProfileScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -37,10 +43,55 @@ fun MainNavigation() {
     
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "login"
     ) {
+        composable("login") {
+            LoginScreen(
+                onNavigateToRegister = { navController.navigate("register") },
+                onNavigateToForgotPassword = { navController.navigate("forgot_password") },
+                onLoginSuccess = { navController.navigate("home") {
+                    popUpTo("login") { inclusive = true }
+                }}
+            )
+        }
+        
+        composable("register") {
+            RegisterScreen(
+                onNavigateToLogin = { navController.popBackStack() },
+                onRegisterSuccess = { navController.navigate("home") {
+                    popUpTo("login") { inclusive = true }
+                }}
+            )
+        }
+        
+        composable("forgot_password") {
+            ForgotPasswordScreen(
+                onNavigateBack = { navController.popBackStack() },
+                onResetSuccess = { navController.popBackStack() }
+            )
+        }
+        
         composable("home") {
-            // TODO: Add HomeScreen
+            HomeScreen(
+                onNavigateToProfile = { navController.navigate("profile") }
+            )
+        }
+        
+        composable("profile") {
+            ProfileScreen(
+                onNavigateBack = { 
+                    if (navController.previousBackStackEntry != null) {
+                        navController.popBackStack()
+                    } else {
+                        navController.navigate("home") {
+                            popUpTo("profile") { inclusive = true }
+                        }
+                    }
+                },
+                onLogout = { navController.navigate("login") {
+                    popUpTo("home") { inclusive = true }
+                }}
+            )
         }
     }
 } 
