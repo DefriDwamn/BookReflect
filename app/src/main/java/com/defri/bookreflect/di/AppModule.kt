5563e0fb1f537c10c858cb5df72720c7ebfea146
Yakov Novashinskii <Emailno@bk.ru>
2025-06-06 01:13:53 +0300

    feat: firebase auth to AppModule

--- app/src/main/java/com/defri/bookreflect/di/AppModule.kt
+++ app/src/main/java/com/defri/bookreflect/di/AppModule.kt
@@ -1,11 +1,23 @@
 package com.defri.bookreflect.di
 
+import com.defri.bookreflect.data.remote.FirebaseAuthSource
+import com.defri.bookreflect.data.repository.AuthRepositoryImpl
+import com.defri.bookreflect.domain.repository.AuthRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    // Здесь будут добавляться провайдеры зависимостей
+    @Provides
+    @Singleton
+    fun provideFirebaseAuthSource(): FirebaseAuthSource = FirebaseAuthSource()
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(firebaseAuthSource: FirebaseAuthSource): AuthRepository =
+        AuthRepositoryImpl(firebaseAuthSource)
 }
