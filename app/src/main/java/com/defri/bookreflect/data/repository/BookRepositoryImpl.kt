467923fd2af9aa5b10fb5d55c411cd286c7b12cf
Yakov Novashinskii <Emailno@bk.ru>
2025-06-10 01:13:28 +0300

    fix: BookRepositoryImpl execptions handle with result

--- app/src/main/java/com/defri/bookreflect/data/repository/BookRepositoryImpl.kt
+++ app/src/main/java/com/defri/bookreflect/data/repository/BookRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.defri.bookreflect.data.repository
 
 import com.defri.bookreflect.data.model.Book
+import com.defri.bookreflect.core.Result
 import com.defri.bookreflect.data.model.BookStatus
 import com.defri.bookreflect.data.remote.FirestoreBookSource
 import com.defri.bookreflect.domain.repository.BookRepository
@@ -10,15 +11,31 @@ class BookRepositoryImpl @Inject constructor(
     private val firestoreBookSource: FirestoreBookSource
 ) : BookRepository {
 
-    override suspend fun addBook(book: Book) {
-        firestoreBookSource.addBook(book)
+    override suspend fun addBook(book: Book): Result<Unit> {
+        return try {
+            firestoreBookSource.addBook(book)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 
-    override suspend fun updateBookStatus(bookId: String, status: BookStatus) {
-        firestoreBookSource.updateBookStatus(bookId, status)
+    override suspend fun updateBookStatus(bookId: String, status: BookStatus): Result<Unit> {
+        return try {
+            firestoreBookSource.updateBookStatus(bookId, status)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 
-    override suspend fun getBooks(): List<Book> {
-        return firestoreBookSource.getBooks()
+    override suspend fun getBooks(): Result<List<Book>> {
+        return try {
+            val books = firestoreBookSource.getBooks()
+            Result.Success(books)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 }
+
