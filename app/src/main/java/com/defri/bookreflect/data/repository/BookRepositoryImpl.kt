44a715df4a4c5e69ec19d3b3711d4c192617f47f
Yakov Novashinskii <Emailno@bk.ru>
2025-06-18 04:07:55 +0300

    feat: add google books to results getGlobalBooksPaged with random genre logic

--- app/src/main/java/com/defri/bookreflect/data/repository/BookRepositoryImpl.kt
+++ app/src/main/java/com/defri/bookreflect/data/repository/BookRepositoryImpl.kt
@@ -7,12 +7,14 @@ import com.defri.bookreflect.data.local.BookDao
 import com.defri.bookreflect.data.mapper.BookMapper
 import com.defri.bookreflect.domain.model.BookStatus
 import com.defri.bookreflect.data.remote.FirestoreBookSource
+import com.defri.bookreflect.data.remote.GoogleBooksSource
 import com.defri.bookreflect.domain.repository.BookRepository
 import javax.inject.Inject
 
 class BookRepositoryImpl @Inject constructor(
     private val firestoreBookSource: FirestoreBookSource,
-    private val dao: BookDao
+    private val dao: BookDao,
+    private val googleBooksSource: GoogleBooksSource
 ) : BookRepository {
 
     override suspend fun createBook(book: Book): Result<Unit> {
@@ -57,7 +59,7 @@ class BookRepositoryImpl @Inject constructor(
                     BookMapper.fromDto(it).copy(isLocal = false)
                 }
             } catch (e: Exception) {
-                Log.e("getUserBooks", "Remote fetch failed: ${e.message}")
+                Log.e("getUserBooks", "fetch failed: ${e.message}")
                 emptyList()
             }
             Result.Success(local + remote)
@@ -68,13 +70,40 @@ class BookRepositoryImpl @Inject constructor(
 
     override suspend fun getGlobalBooksPaged(lastDocumentId: String?, pageSize: Int): Result<List<Book>> {
         return try {
-            val snapshot = firestoreBookSource.getGlobalBooks()
-            val books = snapshot.map { BookMapper.fromDto(it).copy(isLocal = false) }
-            Result.Success(books)
+            val firestoreBooks = try {
+                firestoreBookSource.getGlobalBooksPaged(lastDocumentId, pageSize)
+                    .map { BookMapper.fromDto(it).copy(isLocal = false) }
+            } catch (e: Exception) {
+                emptyList()
+            }
+            val needSize = pageSize - firestoreBooks.size
+            val googleBooks = if (needSize > 0) {
+                try {
+                    val randomQuery = getRandomQuery()
+                    val randomStartIndex = (0..100).random()
+                    googleBooksSource.getBooksPaged(
+                        query = randomQuery,
+                        startIndex = randomStartIndex,
+                        pageSize = needSize
+                    ).map { BookMapper.fromGoogleDto(it) }
+                } catch (e: Exception) {
+                    emptyList()
+                }
+            } else emptyList()
+            Result.Success(firestoreBooks + googleBooks)
         } catch (e: Exception) {
             Result.Error(e)
         }
     }
 
+    private fun getRandomQuery(): String {
+        val queries = listOf(
+            "drama", "fantasy", "fiction", "history",
+            "philosophy", "adventure", "horror"
+        )
+        val count = (1..1).random()
+        val selectedGenres = queries.shuffled().take(count)
+        return "subject:" + selectedGenres.joinToString("+")
+    }
 }
 
